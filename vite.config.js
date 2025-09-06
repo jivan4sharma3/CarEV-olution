9fd402952d0d88e5afabee412c6afa38af4fdfea
Jivan Sharma <jivan4sharma3@gmail.com>
2025-09-06 15:09:54 +0545

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CarEV-olution/', // 👈 must match your GitHub repo name
+  base: '/CarEV-olution/', // 👈 repo name
 })
